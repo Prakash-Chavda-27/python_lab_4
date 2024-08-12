7e92fd81c4e74f3bc74212b1fc543bca3f97ec86
Prakash-Chavda-27 <bca2022Prakash1751@tnraocollege.org>
2024-08-12 12:06:56 +0530

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,16 @@
+emp = {
+    'id': 1,
+    'name': 'Prakash',
+    'designation': 'Manager',
+    'salary': 60000
+}
+
+print("Dictionary:",emp)
+
+# Delete the 'designation' key
+del emp['designation']
+print("\nDeleting Designation:",emp)
+
+# Update the 'name' field
+emp['name'] = 'Vishu Patel'
+print("\nUpdating 'name':",emp)
